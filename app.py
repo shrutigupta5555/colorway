8d82cea2dafdc4a98afb9dac555d65222fce2393
Aakash Shrivastava <69726390+aakzsh@users.noreply.github.com>
2022-06-06 13:46:37 +0530

    add structure

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, send_from_directory
+
+from flask import Flask, render_template, send_from_directory, request
 import os
+from string_random import generate
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,6 +13,19 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    name = ""
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            name = generate()
+            uploaded_file.save(f'static/{name}.jpg')    
+    return render_template('results.html', name = name)
+
+@app.route('/link/<url>')
+def link(url):
+    return render_template('link.html', url=url)
 
 if __name__ == "__main__":
   app.run(debug=True, use_reloader=True)
---
+++ string_random.py
@@ -0,0 +1,10 @@
+import string    
+import random  
+S = 10   
+
+def generate():
+
+    ran = ''.join(random.choices(string.ascii_lowercase, k = S))    
+    return ran
+
+print(generate())
